d87a85c53ca3f88efcf655f20cfcee2292b70911
sa6ok2003 <sa6ok2003@mail.ru>
2021-01-06 12:20:05 +0500

    realize 1

---
+++ config.py
@@ -0,0 +1,26 @@
+TOKEN = '1070685674:AAFSr950QKddJ97GJNf2qAjICFWQSwjo9U8'
+ADMIN1_ID = 494588959
+ADMIN2_ID = 44520977
+
+video_0 = f'Этот смайлик здесь не просто так <a href ="https://t.me/nikola_bonus1/24">😏</a>'
+
+video_1 = f'Когда узнал всю правду <a href ="https://t.me/nikola_bonus2/5">😎</a>'
+
+
+video_2 = f'Один из самых простых способов <a href ="https://t.me/nikola_bonus3/4">🤑</a>'
+
+
+video_3 = f'Оставлю тут <a href ="https://t.me/nikola_bonus4/2">🍰</a>'
+
+
+video_4 = f'Учимся продавать <a href ="https://t.me/nikola_bonus5/3">🎓</a>'
+
+
+video_5 = f'Это тот самый смайлик <a href ="https://t.me/nikola_bonus6/4">🤫</a>'
+
+
+video_6 = f'Не допускай их! <a href ="https://t.me/nikola_bonus7/9">❌</a>'
+
+
+video_7 = f'Начни Зарабатывать <a href ="https://t.me/nikola_bonus8/20">💵</a>'
+
